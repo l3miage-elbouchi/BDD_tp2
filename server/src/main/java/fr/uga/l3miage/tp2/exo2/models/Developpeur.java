41f8a939d6276716125c63a54f1022ebf9c7157c
l3miage-elbouchi <ismail.el-bouch@etu.univ-grenoble-alpes.fr>
2024-03-16 23:28:15 +0100

    fin exo2

---
+++ server/src/main/java/fr/uga/l3miage/tp2/exo2/models/Developpeur.java
@@ -0,0 +1,12 @@
+package fr.uga.l3miage.tp2.exo2.models;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("DEV")
+public class Developpeur extends Employe {
+    String langagePrefere;
+    String niveauCompetence;
+
+}
---
+++ server/src/main/java/fr/uga/l3miage/tp2/exo2/models/Employe.java
@@ -0,0 +1,17 @@
+package fr.uga.l3miage.tp2.exo2.models;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//car c facile a utiliser et la plus utiliser. et on aura une seul table ce qui facilite la modification
+//inconvinion: on aura des null dans notre table
+@DiscriminatorColumn(name="TYPE",length=3)
+
+public class Employe {
+    @Id
+    Long Id;
+    String nom;
+    String email;
+    double salaire;
+}
